b09206585184bf1d1779cad844cd9188f78def11
MarceloGomesFollow <marcelojg@gmail.com>
2025-05-06 15:46:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -172,5 +172,6 @@ PDF SM:
     return jsonify({'report_md': texto, 'grafico': {'datasets': datasets}})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Porta dinâmica vinda do Render
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
+
